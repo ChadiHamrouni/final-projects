d685f164a5707f8c0d319e37ae0536764c981cc8
Chadi Hamrouni <69485266+ChadiHamrouni@users.noreply.github.com>
2021-12-29 01:14:25 +0100

    Post/Get queries before getting top 10 stories

--- hacker-news/server/models/story.js
+++ hacker-news/server/models/story.js
@@ -8,3 +8,48 @@ const story = mongoose.Schema({
 })
 
 module.exports = mongoose.model("Story", story)
+
+
+
+let MongoClient = require('mongodb').MongoClient;
+
+// Specify your local MongoDB instance
+var url = "mongodb://localhost:27017/";
+
+
+// Add a story 
+app.post('/api/story', (req, res) => {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("put database name here");
+        dbo.collection("collection name here").insertOne({
+            id: req.body.id,
+            by: req.body.by,
+            title: req.body.title,
+            score: req.body.score
+        }, 
+        function(err, result) {
+            if (err) throw err;
+            res.json(result);
+            db.close();
+        });
+    });
+});
+// Get all stories
+app.get('/api/stories', (req, res) => {
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("put database name here");
+        dbo.collection("collection name here").findOne({
+            id: req.params.id,
+            by: req.params.by,
+            title: req.params.title,
+            score: req.body.score
+        }, 
+        function(err, result) {
+            if (err) throw err;
+            res.json(result);
+            db.close();
+        });
+    });
+});
